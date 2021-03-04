f9df1f1a6eb39fdc338fa7821cc22f83aa803876
reddusk2021 <80019655+reddusk2021@users.noreply.github.com>
2021-03-04 11:42:53 +0800

    Add files via upload

---
+++ homework2021.java
@@ -0,0 +1,16 @@
+package 寒假作业;
+
+public class homework2021 {
+    public class studennt{
+        String name;
+        int age;
+        int score;
+        public void exam(){
+            int score=(int)(Math.random()*101);
+        }
+    }
+    }
+        public class techer{
+
+        }
+
